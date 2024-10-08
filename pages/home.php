2700806aa04803e44233788a4c7e5ab9aab407a6
William Zelada <zr20005@ti.ues.edu.sv>
2024-10-08 15:13:19 -0600

    a

--- pages/home.php
+++ pages/home.php
@@ -9,6 +9,7 @@
 </head>
 <body>
    
+    <?php include '..\partials\navbar.html'; ?>
     
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
