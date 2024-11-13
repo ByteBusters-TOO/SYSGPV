2e6c35c430d3acfb858239d96c5bf6b7e223e255
michellec20 <cc18064@ues.edu.sv>
2024-11-13 01:30:15 -0600

    FEAT-Registrar Usuario (Validar inserción BD)

---
+++ controllers/usuarioController.php
@@ -0,0 +1,62 @@
+<?php
+session_start();// Inicia una nueva sesión o reanuda la sesión existente
+require_once '../models/mtoUsuario.php';// Incluye el archivo que contiene la definición de la clase 'Usuario'
+
+header('Content-Type: application/json');// Establece el tipo de contenido de la respuesta como JSON
+
+$response = array();// Inicializa un array para almacenar la respuesta
+$method = $_SERVER['REQUEST_METHOD'];// Obtiene el método HTTP de la solicitud actual (GET, POST, PUT, DELETE)
+
+try {
+    $usuarioModel = new Usuario();// Crea una instancia del modelo de 'Usuario'
+
+    switch ($method) {// Verifica el método HTTP de la solicitud y actúa en consecuencia
+        case 'GET':
+            if (isset($_GET['tipo_usuario'])) {// Si la solicitud es GET y se pasa un parámetro 'tipo_usuario', cargara todos los roles del sistema
+                $response = $usuarioModel->getRolesUsuario();
+            } else if (isset($_GET['id'])) {// Si la solicitud es GET y se pasa un parámetro 'id', lee un usuario específico
+                $response = $usuarioModel->read($_GET['id']);
+            } else {// Si no se pasa ningun parametro lee todos los datos de la tabla
+                $response = $usuarioModel->readTable();
+            }
+            break;
+
+        case 'POST':
+            // Decodifica el cuerpo de la solicitud JSON en un array asociativo
+            $data = json_decode(file_get_contents("php://input"), true);
+            // Verifica que se haya pasado los campos requeridos
+            if (!isset($data['nombre_usuario']) || !isset($data['correo_usuario']) ||  !isset($data['id_rol']) || !isset($data['password'])) {
+                throw new Exception('Datos incompletos.');// Si no, lanza una excepción indicando datos incompletos
+            }
+            // Llama al método create del modelo para crear un nuevo Usuario
+            $usuarioModel->create($data['nombre_usuario'], $data['correo_usuario'], $data['id_rol'], $data['password']);
+            $response = ['status' => 'success', 'message' => 'Usuario creado con éxito.'];
+            break;
+
+        case 'PUT':
+            $data = json_decode(file_get_contents("php://input"), true);
+            if (!isset($data['id']) || !isset($data['nombre_usuario']) || !isset($data['correo_usuario']) || !isset($data['id_rol']) || !isset($data['password'])) {
+                throw new Exception('Datos incompletos');// Si no, lanza una excepción indicando datos incompletos
+            }
+            $usuarioModel->update($data['id'], $data['nombre_usuario'], $data['correo_usuario'], $data['id_rol'], $data['password']);
+            $response = ['status' => 'success', 'message' => 'Usuario actualizado con éxito.'];
+            break;
+
+        case 'DELETE':
+            $data = json_decode(file_get_contents("php://input"), true);
+            if (!isset($data['id']) || !isset($data['estado_usuario'])) {
+                throw new Exception('Datos incompletos.');// Si no, lanza una excepción indicando datos incompletos
+            }
+            //$usuarioModel->deshabilitar($data['id'],$data['estado_usuario']);
+            $response = ['status' => 'success', 'message' => 'Usuario deshabilitado con éxito.'];
+            break;
+
+        default:
+            throw new Exception('Método no permitido.');
+    }
+} catch (Exception $e) {
+    $response = ['status' => 'error', 'message' => $e->getMessage()];
+}
+
+echo json_encode($response);
+?>
---
+++ models/mtoUsuario.php
@@ -0,0 +1,80 @@
+<?php
+require_once '../controllers/db_Config.php';//Agregamos la conexion
+
+class Usuario {
+    private $conn;//Variable de conexion
+    private $table_name = "usuario"; //variable que contiene el nombre de la tabla
+
+    //Metodo constructor de la clase
+    public function __construct() {
+        $database = new Database();//Instanciamos la conexion
+        $this->conn = $database->getConnection();//Asignamos la conexion a la variable previamente definida
+    }
+
+    public function readTable(){
+        $query = "SELECT id_usuario,tipo_usuario,nombre_usuario,correo_usuario FROM rol_usuario r, usuario u WHERE r.id_rol = u.id_rol ORDER BY id_usuario";
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    //Funcion que muestra todos los registros de la tabla
+    public function readAll() {
+        $query = "SELECT * FROM " . $this->table_name;
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function read($id) {
+        $query = "SELECT * FROM " . $this->table_name . " WHERE id_usuario = :id";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(':id', $id);
+        $stmt->execute();
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+    public function create($nombre_usuario, $correo_usuario,  $id_rol, $password) {
+        $query = "INSERT INTO " . $this->table_name . " (nombre_usuario, correo_usuario, id_rol, password) VALUES (:nombre_usuario, :correo_usuario, :id_rol, :password)";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(':nombre_usuario', $nombre_usuario);
+        $stmt->bindParam(':correo_usuario', $correo_usuario);
+        $stmt->bindParam(':id_rol', $id_rol);
+        $stmt->bindParam(':password', $password);
+        $stmt->execute();
+    }
+
+    public function update($id, $nombre_usuario, $correo_usuario, $password, $id_rol) {
+        $query = "UPDATE " . $this->table_name . " SET nombre_usuario = :nombre_usuario, correo_usuario = :correo_usuario, password = :password, id_rol = :id_rol WHERE id_usuario = :id_usuario";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(':id', $id);
+        $stmt->bindParam(':nombre_usuario', $nombre_usuario);
+        $stmt->bindParam(':correo_usuario', $correo_usuario);
+        $stmt->bindParam(':password', $password);
+        $stmt->bindParam(':id_rol', $id_rol);
+        $stmt->execute();
+    }
+
+    public function delete($id) {
+        $query = "DELETE FROM " . $this->table_name . " WHERE id_usuario = :id";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(':id', $id);
+        $stmt->execute();
+    }
+
+    /*public function deshabilitar($id,$estado){
+        $query = "UPDATE " . $this->table_name . " SET estado_usuario = :estado WHERE id_usuario = :id";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(':id', $id);
+        $stmt->bindParam(':estado', $estado);
+        $stmt->execute();
+    }*/
+
+    public function getRolesUsuario() {
+        $query = "SELECT id_rol, tipo_usuario FROM rol_usuario";
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
+?>
