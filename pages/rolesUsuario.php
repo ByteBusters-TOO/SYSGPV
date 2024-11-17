c5cec7ccc788c5f1a92aa9374e3813a210dfa667
michellec20 <cc18064@ues.edu.sv>
2024-11-17 10:54:03 -0600

    Modificación en Bootstrap

--- pages/rolesUsuario.php
+++ pages/rolesUsuario.php
@@ -118,7 +118,6 @@
             <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js"></script>
             <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
             <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
             <script src="../js/rol.js"></script>
 
             <!-- Modal de Mensajes -->
